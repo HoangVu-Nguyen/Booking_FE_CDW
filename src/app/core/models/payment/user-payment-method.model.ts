cfd3e867d7949976c9d0da7e823d281e551c8605
HoangVu-Nguyen <hoangvufit2004@gmail.com>
2026-06-10 21:55:07 +0700

    refactor(wallet): withdraw money using card by default.

--- src/app/core/models/payment/user-payment-method.model.ts
+++ src/app/core/models/payment/user-payment-method.model.ts
@@ -11,3 +11,13 @@ export interface UserPaymentMethod {
   isPrimary: boolean;
   status: 'ACTIVE' | 'EXPIRED' | 'LOCKED';
 }
+export interface HostBankAccount {
+  id: number;
+  amount?:number;
+  bankName: string;         // 'Vietcombank', 'Techcombank'
+  bankCode: string;         // 'VCB', 'TCB' để làm logo
+  accountNumber: string;    // '190354678910' -> HIỂN THỊ RÕ ĐỂ ADMIN XEM
+  accountHolderName: string;// 'NGUYEN VAN A'
+  isDefault: boolean;
+
+}
